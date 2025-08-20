91a13b3f2e768433a779dc1bde1a2aa5fa43b7f6
Niran Wangyad <niran.w@cicnetgrp.net>
2025-08-20 20:22:12 +0700

    update

--- backend/theme/views/layouts/main_login.php
+++ backend/theme/views/layouts/main_login.php
@@ -17,26 +17,26 @@ $cururl = Yii::$app->controller->id;
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bbootstrap 4 -->
-    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/jqvmap/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>dist/css/adminlte.min.css">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/summernote/summernote-bs4.css">
     <!-- Select2 -->
-    <link rel="stylesheet" href="plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?= "css/sweetalert.css" ?> ">
@@ -64,45 +64,45 @@ $cururl = Yii::$app->controller->id;
 
 
     <!-- jQuery -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Select2 -->
-    <script src="plugins/select2/js/select2.full.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/select2/js/select2.full.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         // $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="plugins/chart.js/Chart.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="plugins/sparklines/sparkline.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/sparklines/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="plugins/moment/moment.min.js"></script>
-    <script src="plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/moment/moment.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="dist/js/adminlte.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>dist/js/adminlte.js"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="dist/js/pages/dashboard.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>dist/js/pages/dashboard.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="dist/js/demo.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>dist/js/demo.js"></script>
     <!-- Bootstrap Switch -->
-    <script src="plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
-    <script src="js/module_index_delete.js"></script>
-    <script src="js/sweetalert.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>js/module_index_delete.js"></script>
+    <script src="<?= Yii::getAlias('@web') ?>js/sweetalert.min.js"></script>
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -149,9 +149,9 @@ $cururl = Yii::$app->controller->id;
 
 
 <!-- OPTIONAL SCRIPTS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
-<script src="dist/js/demo.js"></script>
-<script src="dist/js/pages/dashboard3.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>plugins/chart.js/Chart.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>dist/js/demo.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>dist/js/pages/dashboard3.js"></script>
 
 </body>
 </html>
