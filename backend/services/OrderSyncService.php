2be026c42b29583212d439744a572d3f8480787c
Niran Wangyad <niran.w@cicnetgrp.net>
2025-08-25 18:03:09 +0700

    update

--- backend/services/OrderSyncService.php
+++ backend/services/OrderSyncService.php
@@ -124,7 +124,7 @@ class OrderSyncService
                     'time_from' => strtotime('-1 day'),
                     'time_to' => time(),
                     'page_size' => $page_size,
-                    'response_optional_fields' => 'order_status,region,total_amount,buyer_user_id',
+                    'response_optional_fields' => 'order_status,total_amount',
                     'order_status' => 'SHIPPED', // optional
                     'cursor' => '',
                 ];
