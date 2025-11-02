96a12cf55576cdd3943edd566b3dd6dda39f8787
Niran Wangyad <niran.w@cicnetgrp.net>
2025-11-02 23:09:41 +0700

    udpate

--- backend/config/main.php
+++ backend/config/main.php
@@ -103,7 +103,8 @@ return [
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+                    'levels' => ['error', 'warning', 'info', 'trace'], // เพิ่ม trace
+                    'logVars' => [],
                 ],
             ],
         ],
