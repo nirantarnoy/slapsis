b451e7878be1a431c35dcc937655dd2ba8879926
Niran Wangyad <niran.w@cicnetgrp.net>
2025-11-30 20:01:33 +0700

    update

--- backend/views/tiktok-income/_pdf.php
+++ backend/views/tiktok-income/_pdf.php
@@ -25,13 +25,11 @@ use yii\helpers\Html;
                 <td colspan="3"><strong>รายได้ (Income)</strong></td>
             </tr>
             <?php foreach ($summary['income'] as $label => $amount): ?>
-            <?php if ($amount != 0): ?>
             <tr>
                 <td style="width: 20px;"></td>
                 <td><?= $label ?></td>
                 <td style="text-align: right;"><?= number_format($amount, 2) ?></td>
             </tr>
-            <?php endif; ?>
             <?php endforeach; ?>
             <tr style="background-color: #dff0d8;">
                 <td colspan="2" style="text-align: right;"><strong>รวมรายได้ (Total Income)</strong></td>
@@ -43,13 +41,11 @@ use yii\helpers\Html;
                 <td colspan="3"><strong>ค่าใช้จ่าย (Expenses)</strong></td>
             </tr>
             <?php foreach ($summary['expense'] as $label => $amount): ?>
-            <?php if ($amount != 0): ?>
             <tr>
                 <td></td>
                 <td><?= $label ?></td>
                 <td style="text-align: right;"><?= number_format($amount, 2) ?></td>
             </tr>
-            <?php endif; ?>
             <?php endforeach; ?>
             <tr style="background-color: #f2dede;">
                 <td colspan="2" style="text-align: right;"><strong>รวมค่าใช้จ่าย (Total Expenses)</strong></td>
